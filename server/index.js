d51e641e7ae37dca21c1b79a4fe2b33aa469a0cb
Mohammed Rediwan <rediwanmuhe@gmail.com>
2024-02-28 09:29:32 +0300

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose.connect(process.env.REALESTATE_DB_URI).then(() => {
+   console.log('Connected to database!');
+}).catch(err => { 
+   console.log(err)
+})
 
 const app = express();
 
-app.listen(3000, () => { 
-   console.log('server listening on port 3000!');
+const port = 8082;
+
+app.listen(port, () => { 
+   console.log(`server listening on port ${port}`);
 })
