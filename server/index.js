62b2584d6daecc3f0f9cc0a21b067f3adb0b49c3
Mohammed Rediwan <rediwanmuhe@gmail.com>
2024-02-29 10:24:31 +0300

    create a middleware and function to handle possible errors

--- server/index.js
+++ server/index.js
@@ -24,3 +24,14 @@ app.listen(port, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/signup', signUpRouter);
+
+// Middleware
+app.use(( err, req, res, next ) => {
+   const statusCode = err.statusCode || 500
+   const message = err.message || 'Internal Server Error'
+   return res.status(statusCode).json({
+      success: false,
+      message,
+      statusCode
+   })
+})
